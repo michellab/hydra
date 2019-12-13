6f32aab8726e2279d816d0dada2c82f3dda68c8e
WilsonWeiWU <wilsonweiwu@gmail.com>
2019-12-13 18:14:47 +0000

    fingerprint similarity preliminary results

--- fingerprint_similarity.py
+++ fingerprint_similarity.py
@@ -1,58 +1,18 @@
 # General:
 import pandas as pd
 import numpy as np
-import os
-import csv
-import subprocess
-import time
-import shutil
-import glob
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
 import seaborn as sns
-import statistics
-import pickle
-
-# SciKit-Optimise:
-import skopt
-from skopt import gp_minimize, forest_minimize
-from skopt.space import Real, Categorical, Integer
-from skopt.plots import plot_convergence
-from skopt.plots import plot_objective, plot_evaluations
-from skopt.utils import use_named_args
 
 # SVM:
-from sklearn.decomposition import PCA
-from sklearn import preprocessing
 from sklearn.model_selection import KFold
-from sklearn.metrics import r2_score
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
 
 # RDKit
 from rdkit import Chem
-from rdkit.Chem import Draw
-from rdkit.Chem import AllChem
-from rdkit.Chem import rdmolfiles, rdMolDescriptors
-from rdkit.Chem import SDMolSupplier, Descriptors, Crippen, Lipinski, Fragments
 from rdkit import DataStructs
 
-# Misc.:
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import IsolationForest
-from sklearn.metrics import mean_absolute_error
-from sklearn.decomposition import PCA
-from sklearn.svm import SVR
-from scipy import stats
-import statistics
-import pickle
-from mordred import Calculator, descriptors
-
-
-# global variables
 absolute_dGoffset_path = './absolute_dGoffset/'
 
-# dataset_path = '~/Dropbox/FreeSolv/dGlearn-FreeSolv-master/datasets/train_compiled/dGhydr_train.csv'
 offset_col_name = 'dGoffset (kcal/mol)'
 
 # set data processing configurations:
@@ -65,12 +25,20 @@ ensemble_size = 10  # Amount of top-scoring models to retain per fold-dataset co
 n_splits = 5  # Number of K-fold splits
 random_state = 2  # Random number seed
 
-train_df = pd.read_csv(absolute_dGoffset_path + 'train_data.csv', index_col='Unnamed: 0')
+train_df_save_loc = absolute_dGoffset_path + 'train_data.csv'
+train_df = pd.read_csv(train_df_save_loc, index_col='Unnamed: 0')
 train_dr = absolute_dGoffset_path + 'train_dr/'
 
+test_df_save_loc = absolute_dGoffset_path + 'test_data.csv'
 test_dr = absolute_dGoffset_path + 'test_dr/'
-test_df = pd.read_csv(absolute_dGoffset_path + 'test_data.csv', index_col='Unnamed: 0')
-test_ID = test_df.index
+test_df = pd.read_csv(test_df_save_loc)
+
+test_df_index_save_loc = absolute_dGoffset_path + 'test_data_index.csv'
+test_df_ID = pd.read_csv(test_df_index_save_loc, index_col='Unnamed: 0')
+test_ID = test_df_ID.index.tolist()
+
+worst_best_df = pd.read_csv(absolute_dGoffset_path + 'worst_best_ligands.csv', index_col='ID')
+worst_best_ID = worst_best_df.index.tolist()
 
 
 def split_dataset(dataset, n_splits, random_state):
@@ -101,36 +69,58 @@ def split_dataset(dataset, n_splits, random_state):
 
 kfolds = split_dataset(train_df, n_splits, random_state)
 
+fold_num = 1
+for mol in worst_best_ID:
 
-# retrieve IDs
-train_IDs = kfolds[0][0][0].index.tolist()
-validate_IDs = kfolds[0][0][1].index.tolist()
-test_IDs = test_ID.tolist()
+    trgt_suppl = Chem.SDMolSupplier(test_dr + str(mol) + '.sdf')
+    trgt_fp = Chem.RDKFingerprint(trgt_suppl[0])
 
-# retrieve SDFs
-train_suppl = [Chem.SDMolSupplier(train_dr + str(sdf) + '.sdf')
-               for sdf in train_IDs]
+    for fold in kfolds:
+        train_IDs = fold[0][0].index.tolist()
+        validate_IDs = fold[0][1].index.tolist()
 
-valdtn_suppl = [Chem.SDMolSupplier(train_dr + str(sdf)  + '.sdf')
-                for sdf in validate_IDs]
+        # retrieve SDFs
+        train_suppl = [Chem.SDMolSupplier(train_dr + str(sdf) + '.sdf')
+                       for sdf in train_IDs]
 
-test_suppl = [Chem.SDMolSupplier(test_dr + str(sdf)  + '.sdf')
-              for sdf in test_IDs]
+        valdtn_suppl = [Chem.SDMolSupplier(train_dr + str(sdf) + '.sdf')
+                        for sdf in validate_IDs]
 
+        # generate fingerprints
+        train_fp = [Chem.RDKFingerprint(mol[0]) for mol in train_suppl]
+        valdtn_fp = [Chem.RDKFingerprint(mol[0]) for mol in valdtn_suppl]
 
-# generate fingerprints
-train_fp = [Chem.RDKFingerprint(mol) for mol in train_suppl]
-valdtn_fp = [Chem.RDKFingerprint(mol) for mol in valdtn_suppl]
-test_fp = [Chem.RDKFingerprint(mol) for mol in test_suppl]
+        # compute similarities
+        train_similarity = [DataStructs.FingerprintSimilarity(trgt_fp, train_mol)
+                            for train_mol in train_fp]
 
+        valdnt_similarity = [DataStructs.FingerprintSimilarity(trgt_fp, valdnt_mol)
+                            for valdnt_mol in valdtn_fp]
 
-# compare fingerprints
-test_train_similarity = [DataStructs.FingerprintSimilarity(test_mol, train_mol)
-                         for test_mol in test_fp
-                         for train_mol in train_fp]
+        plt.figure()
 
-test_valdtn_similarity = [DataStructs.FingerprintSimilarity(test_mol, valdtn_mol)
-                          for test_mol in test_fp
-                          for valdtn_mol in valdtn_fp]
+        # density plot
+        sns.distplot(train_similarity,
+                     hist=False,
+                     kde=True,
+                     kde_kws={'linewidth': 3},
+                     label='Train similarity')
 
-print(test_train_similarity)
+        sns.distplot(valdnt_similarity,
+                     hist=False,
+                     kde=True,
+                     kde_kws={'linewidth': 3},
+                     label='Validation similarity')
+
+        title = 'Fold ' + str(fold_num)
+
+        plt.title(title)
+        plt.xlabel('Fingerprint similarity')
+        plt.ylabel('Frequency')
+        plt.legend()
+        # plt.show()
+        save_loc = absolute_dGoffset_path + 'fold_' + str(fold_num) + '_fp_hist.png'
+        plt.savefig(save_loc)
+
+        fold_num += 1
+
