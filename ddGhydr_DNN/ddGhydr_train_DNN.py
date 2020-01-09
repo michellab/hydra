b0a364fc4f5bc463f585877bf7e33209d2444627
WilsonWeiWU <wilsonweiwu@gmail.com>
2020-01-09 08:57:43 +0000

    Using 3 GPUs only.

--- ddGhydr_DNN/ddGhydr_train_DNN.py
+++ ddGhydr_DNN/ddGhydr_train_DNN.py
@@ -3,15 +3,21 @@ import pandas as pd
 import numpy as np
 import os
 import csv
+import sys
 import time
 import shutil
 import pickle
 import logging
+from scipy import stats
 
-# Tensorflow
+# Tensorflow:
 import tensorflow as tf
 from tensorflow import keras
-from scipy import stats
+os.environ["CUDA_VISIBLE_DEVICES"]="1, 2, 3"	# current workstation contains 4 GPUs; exclude 1st
+# https://stackoverflow.com/questions/55081911/tensorflow-2-0-0-alpha0-tf-logging-set-verbosity
+# logging.getLogger("tensorflow").setLevel(logging.ERROR)
+# https://stackoverflow.com/questions/35869137/avoid-tensorflow-print-on-standard-error
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 3 = INFO, WARNING, and ERROR messages are not printed
 
 # Sklearn
 from skopt import gp_minimize
@@ -409,11 +415,53 @@ def save_csv(dataframe, pathname):
     print('Completed writing {}.csv.'.format(pathname))
 
 
+# https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 if __name__ == '__main__':
 
     print('––––––––––––––––––––––––––––––––––––––––––––––––')
     print('––––––––––––––––––––––––––––––––––––––––––––––––')
     print('Script started on {}'.format(time.ctime()))
+
+    # prevent accidental clearance of previous models in output/
+    question = 'Are you sure you want to clear saved models in ./output/ ??'
+    query = query_yes_no(question, 'no')
+    if not query:
+        print('Script terminated.')
+        sys.exit()
+    elif query:
+        print('./output/ cleared.')
 
     # clean slate output_dr
     if os.path.exists(output_dr):
@@ -422,6 +470,7 @@ if __name__ == '__main__':
         os.mkdir(output_dr)
 
     main()
+
     print('––––––––––––––––––––––––––––––––––––––––––––––––')
     print('––––––––––––––––––––––––––––––––––––––––––––––––')
     print('Script finished.')
